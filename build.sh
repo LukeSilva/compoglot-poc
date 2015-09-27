13c93c15b9abae79c9dc7a2fb0ea0636b66de74c
Cool As <goldenscruff@gmail.com>
2015-09-27 20:00:07 +1000

    Fixed buildscript to compile German Core

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ CFLAGS="-Wall -std=c++11"
 BASEFILES="cmdparse.cpp fileparse.cpp main.cpp Language.cpp general.cpp"
 PARSERFILES="parser.cpp parser_io.cpp parse_noun.cpp"
 EN_OLDFILES="en_old.cpp en_old_nouns.cpp en_old_articles.cpp en_old_verbs.cpp en_old_adjectives.cpp en_adverbs.cpp en_old_prepos.cpp en_old_conjunctions.cpp en_general.cpp"
-DEFILES="de.cpp de_articles.cpp de_nouns.cpp de_verbs.cpp de_prepos.cpp de_adjectives.cpp de_conjunctions.cpp de_general.cpp"
+DEFILES="de.cpp"
 ENFILES="en.cpp en_nouns.cpp en_articles.cpp en_numerals.cpp en_verbs.cpp en_passiveverbs.cpp en_prepos.cpp en_adjectives.cpp en_conjunctions.cpp"
 
 echo
@@ -32,6 +32,17 @@ do
 	echo $file
 	g++ $CFLAGS -c en/$file
 done
+
+echo
+echo "Compiling de..."
+echo
+for file in $DEFILES
+do
+  echo $file
+  g++ $CFLAGS -c de/$file
+done
+
+
 
 echo
 echo "Linking..."
