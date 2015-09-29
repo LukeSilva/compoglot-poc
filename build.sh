f00d47c0ec91d1ca2b749818fe86501e7295d691
CoolAs <goldenscruff@gmail.com>
2015-09-30 08:07:51 +1000

    Modified build.sh to allow user to only (re)build required segments of compoglot.

--- build.sh
+++ build.sh
@@ -1,54 +1,85 @@
-cd source
 CFLAGS="-Wall -std=c++11"
 BASEFILES="cmdparse.cpp fileparse.cpp main.cpp Language.cpp general.cpp"
 PARSERFILES="parser.cpp parser_io.cpp parse_noun.cpp"
 EN_OLDFILES="en_old.cpp en_old_nouns.cpp en_old_articles.cpp en_old_verbs.cpp en_old_adjectives.cpp en_adverbs.cpp en_old_prepos.cpp en_old_conjunctions.cpp en_general.cpp"
-DEFILES="de.cpp de_nouns.cpp"
+DEFILES="de.cpp de_nouns.cpp de_articles.cpp"
 ENFILES="en.cpp en_nouns.cpp en_articles.cpp en_numerals.cpp en_verbs.cpp en_passiveverbs.cpp en_prepos.cpp en_adjectives.cpp en_conjunctions.cpp"
+FILEPREFIX="../source/"
+base()
+{
+	echo
+	echo "Compiling base..."
+	echo
+	for file in $BASEFILES
+	do
+		echo $file
+		g++ $CFLAGS -c "${FILEPREFIX}$file"
+	done
+}
 
-echo
-echo "Compiling base..."
-echo
-for file in $BASEFILES
-do
-	echo $file
-	g++ $CFLAGS -c $file
-done
+parser()
+{
+	echo
+	echo "Compiling parser..."
+	echo
+	for file in $PARSERFILES
+	do
+		echo $file
+		g++ $CFLAGS -c "${FILEPREFIX}parser/$file"
+	done
+}
 
-echo
-echo "Compiling parser..."
-echo
-for file in $PARSERFILES
-do
-	echo $file
-	g++ $CFLAGS -c parser/$file
-done
+en()
+{
+	echo
+	echo "Compiling en..."
+	echo
+	for file in $ENFILES
+	do
+		echo $file
+		g++ $CFLAGS -c "${FILEPREFIX}en/$file"
+	done
+}
 
-echo
-echo "Compiling en..."
-echo
-for file in $ENFILES
-do
-	echo $file
-	g++ $CFLAGS -c en/$file
-done
+de()
+{
+	echo
+	echo "Compiling de..."
+	echo
+	for file in $DEFILES
+	do
+		echo $file
+		g++ $CFLAGS -c "${FILEPREFIX}de/$file"
+	done
+}
 
-echo
-echo "Compiling de..."
-echo
-for file in $DEFILES
-do
-  echo $file
-  g++ $CFLAGS -c de/$file
-done
+clean()
+{
+	echo
+	echo "Cleaning up..."
+	echo
+	rm *.o
+}
 
+mkdir -p build
+cd build
 
+PART=$1
+if [[ -z "$PART" ]]
+then
+	base
+	parser
+	en
+	de
+else
+	$PART
+fi
 
-echo
-echo "Linking..."
-echo
-g++ -o lang *.o
-
-echo "Clean Up..."
-rm *.o
-cp lang ../examples/
+if [[ "$PART" != "clean" ]]
+then
+	echo
+	echo "Linking..."
+	echo
+	g++ -o lang *.o
+	cp lang ../examples/
+fi
