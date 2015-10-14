9dc5e602cf597d8dddc03b42a776f9c838716e15
CoolAs <goldenscruff@gmail.com>
2015-10-15 10:28:10 +1100

    Working on documentation, started noun list.
    Verb 2 now functions for esperanto.
    Added vocab to esperanto and german for noun list.
    Noun list lists all nouns that is supported in all language cores.
    Verb list is up to verb 2 for esperanto

--- doc.sh
+++ doc.sh
@@ -2,6 +2,10 @@ cd examples
 ./compoglot file de verblist.htm verblist.de.htm
 ./compoglot file en verblist.htm verblist.en.htm
 ./compoglot file eo verblist.htm verblist.eo.htm
+./compoglot file de nounlist.htm nounlist.de.htm
+./compoglot file en nounlist.htm nounlist.en.htm
+./compoglot file eo nounlist.htm nounlist.eo.htm
 cd ..
 mv examples/verblist.*.htm documentation/
+mv examples/nounlist.*.htm documentation/
 
