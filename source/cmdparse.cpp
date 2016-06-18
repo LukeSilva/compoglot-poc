12d203df5b97ae148699aa619fe15d570d48f7c0
CoolAs <goldenscruff@gmail.com>
2016-06-18 22:34:48 +1000

    Cmdline parser now correctly prints error messages

--- source/cmdparse.cpp
+++ source/cmdparse.cpp
@@ -1,15 +1,25 @@
 #include "Language.h"
 #include "parser/Parser.h"
+#include "parser/ParseException.h"
 #include "settings.h"
 #include <iostream>
 int cmdParse(int argc,char* argv[]){
- if (argc!=4)
- {
-  std::cout << "Usage:" << argv[0] << " " << argv[1] << " <language> <string-to-parse>" << std::endl;;
-  return -1;
- }
- Parser parser;
- std::cout << parser.parse(std::string(argv[2]),std::string(argv[3]));
- std::cout << std::endl;
- return 0;
+	if (argc!=4)
+	{
+		std::cout << "Usage:" << argv[0] << " " << argv[1] << " <language> <string-to-parse>" << std::endl;;
+		return -1;
+	}
+
+	Parser parser;
+	try
+	{
+		std::cout << parser.parse(std::string(argv[2]),std::string(argv[3])) << std::endl;
+	}
+	catch(ParseException& e)
+	{
+		std::cerr << e;
+		return -1;
+	}
+
+	return 0;
 }
